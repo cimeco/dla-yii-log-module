da8d1f4e9a0e16e9148842d49a80c89ab802b980
Janet <jperez@quoma.com.ar>
2018-08-10 10:20:41 -0300

    Model log

---
+++ db/ActiveRecord.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\modules\log\db;
+
+use app\modules\log\src\models\Log;
+
+class ActiveRecord extends \quoma\core\db\ActiveRecord
+{
+    
+    public function init()
+    {
+        parent::init();
+        $this->on( ActiveRecord::EVENT_AFTER_INSERT, function ($event) {
+            $this->createLog($event);
+        });
+        $this->on( self::EVENT_AFTER_UPDATE, function ($event) {
+            $this->createLog($event);
+        });
+        $this->on( ActiveRecord::EVENT_AFTER_DELETE, function ($event) {
+            $this->createLog($event);
+        });
+    }
+    
+    public function getDeletable(){
+        
+        return false;
+        
+    }
+    
+    public function createLog($event){
+        $senderClass = get_class($event->sender);
+        $keys = array_keys($this->attributes);
+        foreach($keys as $key){
+            if(array_key_exists($key, $this->attributes) && array_key_exists($key, $event->changedAttributes)){
+                if($this->attributes[$key] != $event->changedAttributes[$key]){
+                    Log::log($senderClass, $this->primaryKey, $key, $event->changedAttributes[$key], $this->attributes[$key]);
+                }
+            }
+        }
+    }
+}
--- src/models/Log.php
+++ src/models/Log.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace quoma\modules\log\models;
+namespace app\modules\log\src\models;
 
 use Yii;
+use app\modules\LogModule;
 
 /**
  * This is the model class for table "log".
@@ -14,15 +15,17 @@ use Yii;
  * @property string $model
  * @property integer $model_id
  * @property string $data
+ * @property string $attribute
+ * @property string $post
+ * @property string $get
  *
  * @property LogData[] $logDatas
  */
 class Log extends \quoma\core\db\ActiveRecord
 {
 
-    /**
-     * @inheritdoc
-     */
+    static $ignoreData = ['_csrf', 'r'];
+
     public static function tableName()
     {
         return 'log';
@@ -35,7 +38,7 @@ class Log extends \quoma\core\db\ActiveRecord
     {
         return Yii::$app->get('dblog');
     }
-    
+
     /**
      * @inheritdoc
      */
@@ -57,9 +60,10 @@ class Log extends \quoma\core\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'datetime'], 'integer'],
-            [['route'], 'string', 'max' => 100],
-            [['model'], 'string', 'max' => 45]
+            [['user_id', 'datetime', 'model_id'], 'integer'],
+            [['route', 'attribute'], 'string', 'max' => 100],
+            [['model'], 'string', 'max' => 45],
+            [['old_value', 'new_value', 'post', 'get'], 'safe']
         ];
     }
 
@@ -69,13 +73,18 @@ class Log extends \quoma\core\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'log_id' => LogMoudle::t('Log ID'),
-            'route' => LogMoudle::t('Route'),
-            'user_id' => LogMoudle::t('User ID'),
-            'datetime' => LogMoudle::t('Datetime'),
-            'model' => LogMoudle::t('Model'),
+            'log_id' => LogModule::t('Log ID'),
+            'route' => LogModule::t('Route'),
+            'user_id' => LogModule::t('User ID'),
+            'datetime' => LogModule::t('Datetime'),
+            'class' => LogModule::t('Class'),
+            'model_id' => LogModule::t('Model ID'),
+            'old_value' => LogModule::t('Old Value'),
+            'new_value' => LogModule::t('New Value'),
+            'post' => LogModule::t('Post'),
+            'get' => LogModule::t('Get'),
         ];
-    }    
+    }
 
     /**
      * @inheritdoc
@@ -83,26 +92,28 @@ class Log extends \quoma\core\db\ActiveRecord
      */
     public function getDeletable()
     {
-        if($this->getLogData()->exists()){
+        if ($this->getLogData()->exists()) {
             return false;
         }
         return true;
     }
-    
+
     /**
      * @brief Deletes weak relations for this model on delete
      * Weak relations: None.
      */
-    protected function unlinkWeakRelations(){
+    protected function unlinkWeakRelations()
+    {
+        
     }
-    
+
     /**
      * @inheritdoc
      */
     public function beforeDelete()
     {
         if (parent::beforeDelete()) {
-            if($this->getDeletable()){
+            if ($this->getDeletable()) {
                 $this->unlinkWeakRelations();
                 return true;
             }
@@ -110,45 +121,82 @@ class Log extends \quoma\core\db\ActiveRecord
             return false;
         }
     }
-    
-    public static function log($model = null, $model_id = null, $data = null)
+
+    public static function log($model = null, $model_id = null, $attribute = null, $old_value = null, $new_value = null, $data = null, $post = null, $get = null)
     {
-        
+
         $log = new Log();
         $log->route = Yii::$app->requestedRoute ? Yii::$app->requestedRoute : 'site/index';
         $log->user_id = Yii::$app->user->getId();
-        
         $log->model = $model;
         $log->model_id = $model_id;
-        
-        $log->data = $data;
-        
-        return $log->save();
-        
+        $log->attribute = $attribute;
+        $log->old_value = $old_value;
+        $log->new_value = $new_value;
+
+        if (empty($post)) {
+            $post = self::prettyArray(Yii::$app->request->getBodyParams());
+            if (Yii::$app->request->post()) {
+                $post .= "\n\nDatos:\n" . self::prettyArray(Yii::$app->request->post());
+            }
+        }
+
+        if (empty($get)) {
+            $get = self::prettyArray(Yii::$app->request->getQueryParams());
+            if (Yii::$app->request->get()) {
+                $get .= "\n\nDatos:\n" . self::prettyArray(Yii::$app->request->get());
+            }
+        }
+
+        $log->post = $post;
+        $log->get = $get;
+
+        $log->save();
+        \Yii::trace($log->getErrors());
     }
-    
-    public function afterSave($insert, $changedAttributes) {
+
+    private static function prettyArray($data)
+    {
+        $pretty = "";
+
+        if (is_array($data)) {
+            foreach ($data as $k => $d) {
+                if (!in_array($k, self::$ignoreData) && !empty($d)) {
+                    $pretty .= "$k: ";
+                    if (is_array($d)) {
+                        $pretty .= "\n";
+                    }
+                    $pretty .= self::prettyArray($d);
+                }
+            }
+        } else {
+            $pretty .= "$data\n";
+        }
+
+        return $pretty;
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
         Log::garbageCollector();
     }
-    
+
     /**
      * Si se ejecuta en cada afterSave, siempre eliminara 1 solo item luego
      * de haber superado el limite especificado
      */
     public static function garbageCollector()
     {
-        
+
         $limit = Yii::$app->params['garbageCollectorLimit'];
         $count = Log::find()->count();
-        if($count > $limit){
-            
+        if ($count > $limit) {
+
             $qty = $count - $limit;
 
-            Yii::$app->dblog->createCommand('DELETE FROM log ORDER BY log_id ASC LIMIT '.$qty)
-                ->execute();
-            
+            Yii::$app->dblog->createCommand('DELETE FROM log ORDER BY log_id ASC LIMIT ' . $qty)
+                    ->execute();
         }
-        
     }
-    
+
 }
